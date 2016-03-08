efda0a364a9dc74b4b3f53b7bbbaa9fbff63749e
komendacki <artur.komendacki@gmail.com>
2016-03-08 13:49:16 +0200

    Added main classes

---
+++ src/com/my/list/MyList.java
@@ -0,0 +1,43 @@
+package com.my.list;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by Admin on 08.03.2016.
+ */
+public class MyList<T> {
+
+    public int count;
+
+    public void add(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public boolean containts(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void copyTo(T[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public boolean isReadOnly()
+    {
+        throw new NotImplementedException();
+    }
+
+    public boolean remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
---
+++ src/com/my/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Admin on 08.03.2016.
+ */
+package com.my;
